31216efbc0f1c990baaf8e2ccba2108fd549eaf9
Rose Mary <rosemaryjoe007@gmail.com>
2026-04-21 19:23:54 +0530

    feat:CRUD operations

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,10 @@ const app=express();
 
 app.use(express.json());
 import userRoutes from "./routes/user.route.js";
-
+import postRoutes from "./routes/post.router.js";
 
 
 app.use("/api/users",userRoutes);
-
+app.use("/api/posts",postRoutes);
 
 export default app;
